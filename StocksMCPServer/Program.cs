f47b53cde4a6b2d039f7bdab1a218c832ea3679b
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2025-04-20 18:53:24 +0300

    Add Stock performance server

---
+++ StocksMCPServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StocksMCPServer.Tools;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<StockTools>();
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services.AddSingleton(_ =>
+{
+    var client = new HttpClient();
+    return client;
+});
+
+await builder.Build().RunAsync();
---
+++ StocksMCPServer/Tools/StockTools.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Logging;
+
+namespace StocksMCPServer.Tools;
+
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Text.Json;
+
+
+[McpServerToolType]
+public sealed class StockTools
+{
+    private static readonly string ALPHA_VANTAGE_API_KEY = Environment.GetEnvironmentVariable("ALPHA_VANTAGE_API_KEY");
+
+    [McpServerTool, Description("Get portfolio performance.")]
+    public static async Task<string> GetPerformance(
+        HttpClient client,
+        [Description("Stock symbols")] string[] stocks)
+    {
+
+        List<string> data = await GetPortfolioPerformance(client, stocks);
+        return string.Join(", ", data);
+    }
+    
+    private static async Task<string> GetStockPrice(HttpClient client, string symbol)
+            {
+                var url = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}";
+                var response = await client.GetStringAsync(url);
+                var data = JsonDocument.Parse(response);
+    
+                var globalQuote = data.RootElement.GetProperty("Global Quote");
+                globalQuote.TryGetProperty("10. change percent", out var changePercent);
+                if (string.IsNullOrEmpty(changePercent.ToString())) return "";
+                return symbol + ": " + changePercent;
+              
+            }
+
+    private static async Task<List<string>> GetPortfolioPerformance(HttpClient client, string[] symbols)
+    {
+        List<string> portfolio = [];
+
+        foreach (var symbol in symbols)
+        {
+             var stockData = await GetStockPrice(client, symbol);
+             portfolio.Add(stockData);
+            
+        }
+
+        return portfolio;
+    }
+}
