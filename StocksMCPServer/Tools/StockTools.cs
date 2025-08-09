658b04da3286d193c784602c31c665cbbc4c954c
peppial <info@ermena.com>
2025-08-09 18:30:14 +0200

    Update ModelContextProtocol package version and refactor GetPerformance method return type to use structured content

--- StocksMCPServer/Tools/StockTools.cs
+++ StocksMCPServer/Tools/StockTools.cs
@@ -12,28 +12,28 @@ public sealed class StockTools
 {
     private static readonly string ALPHA_VANTAGE_API_KEY = Environment.GetEnvironmentVariable("ALPHA_VANTAGE_API_KEY");
 
-    [McpServerTool, Description("Get portfolio performance.")]
-    public static async Task<string> GetPerformance(
+    [McpServerTool(UseStructuredContent = true), Description("Get portfolio performance.")]
+    public static async Task<object> GetPerformance(
         HttpClient client,
         [Description("Stock symbols")] string[] stocks)
     {
 
         List<string> data = await GetPortfolioPerformance(client, stocks);
-        return string.Join(", ", data);
+        return data;
     }
-    
+
     private static async Task<string> GetStockPrice(HttpClient client, string symbol)
-            {
-                var url = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}";
-                var response = await client.GetStringAsync(url);
-                var data = JsonDocument.Parse(response);
-    
-                var globalQuote = data.RootElement.GetProperty("Global Quote");
-                globalQuote.TryGetProperty("10. change percent", out var changePercent);
-                if (string.IsNullOrEmpty(changePercent.ToString())) return "";
-                return symbol + ": " + changePercent;
-              
-            }
+    {
+        var url = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}";
+        var response = await client.GetStringAsync(url);
+        var data = JsonDocument.Parse(response);
+
+        var globalQuote = data.RootElement.GetProperty("Global Quote");
+        globalQuote.TryGetProperty("10. change percent", out var changePercent);
+        if (string.IsNullOrEmpty(changePercent.ToString())) return "";
+        return symbol + ": " + changePercent;
+
+    }
 
     private static async Task<List<string>> GetPortfolioPerformance(HttpClient client, string[] symbols)
     {
@@ -41,9 +41,9 @@ public sealed class StockTools
 
         foreach (var symbol in symbols)
         {
-             var stockData = await GetStockPrice(client, symbol);
-             portfolio.Add(stockData);
-            
+            var stockData = await GetStockPrice(client, symbol);
+            portfolio.Add(stockData);
+
         }
 
         return portfolio;
